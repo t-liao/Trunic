1e34ba9d0d040b7cac92e9ac3dc6002609028fa2
Teresa Liao <ter92240@gmail.com>
2023-06-12 04:35:06 -0700

    added pop up image of correct rune when question mark button is pressed

--- app/src/main/java/com/example/trunic/Practice.java
+++ app/src/main/java/com/example/trunic/Practice.java
@@ -1,14 +1,24 @@
 package com.example.trunic;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import java.util.Locale;
 
 public class Practice extends AppCompatActivity {
     private DBHelper dbHelper;
@@ -126,13 +136,48 @@ public class Practice extends AppCompatActivity {
             }
         });
 
+
         //func to popup img of solution
         Button idkButton = (Button) findViewById(R.id.questionButton);
         idkButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // inflate the layout of the popup window
+                LayoutInflater inflater = (LayoutInflater)
+                        getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = inflater.inflate(R.layout.popup_image, null);
 
+                //set correct img
+                String runeName = phonemeText.getText().toString().toLowerCase(Locale.ENGLISH);
+                if (runeName.equals("th") && symbolText.getText().toString().equals("θ")){
+                    runeName = runeName + "__";
+                } else if (runeName.equals("th") && symbolText.getText().toString().equals("ð")) {
+                    runeName = runeName + "___";
+                }
 
+                ImageView runeImg = (ImageView) popupView.findViewById(R.id.imageRune);
+                int resourceId = getResources().getIdentifier(
+                        runeName, "drawable", getPackageName() );
+                runeImg.setImageResource(resourceId);
+
+                // create the popup window
+                int width = ConstraintLayout.LayoutParams.MATCH_PARENT;
+                int height = ConstraintLayout.LayoutParams.MATCH_PARENT;
+                boolean focusable = true; // lets taps outside the popup also dismiss it
+                final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+                // show the popup window
+                // which view you pass in doesn't matter, it is only used for the window token
+                popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+
+                // dismiss the popup window when touched
+                popupView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        popupWindow.dismiss();
+                        return true;
+                    }
+                });
             }
         });
 
